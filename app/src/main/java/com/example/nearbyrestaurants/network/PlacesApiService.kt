761cde161d8a8d4c4694547b8f6fb97e99bd7c53
mspriyakk <mspriyakk@gmail.com>
2023-11-18 19:34:42 -0500

    nearby restaurants

---
+++ app/src/main/java/com/example/nearbyrestaurants/network/PlacesApiService.kt
@@ -0,0 +1,17 @@
+package com.example.nearbyrestaurants.network
+/*
+private const val BASE_URL = "https://maps.googleapis.com/maps/api/place/"
+interface PlacesApiService {
+    @GET ("nearbysearch/json") suspend fun getPlaces(
+        @Query("location") location: String,@Query("radius") radius: Int,@Query("type") type: String,@Query("key") apikey: string):Response
+    private val json =Json{
+        ignoreUnknownKeys = true}
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE URL)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType())).build()
+    object PlacesApi {
+        val retrofitService :PlacesApiService by lazy {
+            retrofit.create(PlacesApiService::class.java)
+        }
+
+ */
